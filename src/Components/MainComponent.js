a7d6444a6f80468b90c2d4437e3a7acffef4c6a4
harsh788 <Harsh.Shah@iiitb.org>
2022-07-22 22:32:07 +0530

    ignore

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
-<<<<<<< HEAD
-=======
 import Home from './HomeComponent';
->>>>>>> master
 
 class Main extends Component {
     render() {
         return (
             <div> 
                 <Header />
-<<<<<<< HEAD
-=======
                 <Home />
->>>>>>> master
                 <Footer />
             </div>
         );
