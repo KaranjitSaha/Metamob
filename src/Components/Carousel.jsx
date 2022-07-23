815c759e9a61f0823e98a9dc1c0067d45d60b563
Karanjit Saha <karanjitsaha@pop-os.localdomain>
2022-07-23 11:08:56 +0530

    Solved image rendering(partial)

--- src/Components/Carousel.jsx
+++ src/Components/Carousel.jsx
@@ -1,39 +1,41 @@
+/* IMPORTING MODULES */
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles.css";
 import Slider from "react-slick";
+
+/* IMPORTING CSS */
+import "bootstrap/dist/css/bootstrap.min.css";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import { isTemplateSpan } from "typescript";
-
+import "../styles.css";
+// import '../../
 const images = [{
     id: 1,
     title: "Stop the amazon massacre",
-    src: "../images/IMG_1232.JPG",
-    alt:"photo1",
+    src: "../../public/logo192.png",
+    alt: "photo1",
 },
 {
     id: 2,
-    title:"Restore the rain forest in Brazil",
+    title: "Restore the rain forest in Brazil",
     src: "/home/karanjitsaha/Desktop/Metamob/images/IMG_1232.JPG",
     alt: "Image 2 "
 },
-// {
-//     id: 3,
-//     src: "public/logo192.png",
-//     alt: "Image 3"
-// }
+    // {
+    //     id: 3,
+    //     src: "public/logo192.png",
+    //     alt: "Image 3"
+    // }
 ];
 
 export default function Carousel() {
     const settings = {
         infinite: true,
         dots: true,
+        fade: true,
         slidesToShow: 1,
         slidesToScroll: 1,
-        lazyLoad: true,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 1000,
 
     };
     return (
@@ -43,12 +45,26 @@ export default function Carousel() {
             </div>
             <div className="imgslider">
                 <Slider {...settings}>
-                    {images.map((item) => (
+                    <div>
+                        <img src={"logo192.png"} />
+                        <h1>Stop the amazon massacre</h1>
+                    </div>
+                    <div>
+                        <img src={require("./png-transparent-multicolored-balloons-illustration-balloon-balloon-free-balloons-easter-egg-desktop-wallpaper-party-thumbnail.png").default} />
+                        <h1>Stop the amazon massacre</h1>
+                    </div>
+                    <div>
+                        <img src={"logo192.png"} />
+                        <h1>Stop the amazon massacre</h1>
+                    </div>
+
+                    {images.map((item) => {
                         <div key={item.id}>
-                            <img src={item.src} alt={item.alt} />
+                            <img src={item.src} alt={item.alt}></img>
                             <h1>{item.title}</h1>
                         </div>
-                    ))}
+                    })}
+
                 </Slider>
             </div>
         </>
