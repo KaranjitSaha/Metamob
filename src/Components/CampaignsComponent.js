3a642bcd3dc1e0fe5ea93e1039fc74b5bf2fa8fb
harsh788 <Harsh.Shah@iiitb.org>
2022-07-24 01:04:23 +0530

    Campaigns header

--- src/Components/CampaignsComponent.js
+++ src/Components/CampaignsComponent.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Input } from 'reactstrap';
 
 class Campaigns extends Component {
     constructor(props) {
@@ -7,8 +8,35 @@ class Campaigns extends Component {
 
     render() {
         return (
-            <div>
-
+            <div className="container">
+                <div className="row justify-content-center">
+                    <div className="col-12 col-sm-2">
+                        <Button className="btn"><span className="fa fa-fire"></span></Button>
+                        <Button className="btn"><span className="fa fa-clock-o"></span></Button>
+                    </div>
+                    <div className="col-12 col-sm-3">
+                        <Input type="text" id="title" placeholder="Title" style={{margin: "5px"}}/>
+                    </div>
+                    <div className="col-12 col-sm-2">
+                        <Input type="select" id="select" name="select" style={{margin: "5px", padding: "7px"}}>
+                            <option>All</option>
+                            <option>Animals</option>
+                            <option>Enviroment</option>
+                            <option>General</option>
+                            <option>Government</option>
+                            <option>Human Rights</option>
+                        </Input>
+                    </div>
+                    <div className="col-12 col-sm-3">
+                        <Input type="text" id="any" placeholder="Any" style={{margin: "5px"}}/>
+                    </div>
+                    <div className="col-12 col-sm-1">
+                        <Button className="btn btn-success"><span className="fa fa-search"></span></Button>
+                    </div>
+                    <div className="col-12 col-sm-2 offset-11">
+                        <Button className="btn btn-success"><span className="fa fa-plus"></span> Create</Button>
+                    </div>
+                </div>
             </div>
         )
     }
