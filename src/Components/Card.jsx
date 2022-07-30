c1a18dc057279f5677a5cd9a381b5d81ccdb7f93
Karanjit Saha <karanjitsaha@pop-os.localdomain>
2022-07-30 13:47:38 +0530

    connected to metamask

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -10,11 +10,6 @@ import "../styles.css";
 
 export default function CardDisplay(props) {
     return (
-        // <div key={props.item.id} style={{backgroundColor:"white", borderRadius:"10px", borderStyle:"outset",borderWidth:"3px",marginTop:"3", marginTop: "3rem"}} height="600rem" width="600rem">
-        // {/* TODO: height="600rem" width="400rem" fix the size of this div and make the div to be somewhat raised */}
-        //     <img src={props.item.src.default} alt={props.item.alt} height="400rem" width="534rem" style={{borderRadius:"10px"}}></img>
-        //     <h1>{props.item.title}</h1>
-        // </div>
         <Card className="shadow-sm p-3 mb-5 bg-white rounded">
             <CardImg src={props.item.src.default} alt={props.item.alt} style={{height: "30rem"}}/>
             <CardBody>
--- src/Components/HeaderComponent.js
+++ src/Components/HeaderComponent.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Navbar, Nav, NavbarBrand, NavItem, NavbarToggler, Collapse, Button } from "reactstrap";
 import { NavLink } from "react-router-dom";
 
@@ -6,53 +6,81 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isNavOpen: false
+      isNavOpen: false,
+      walletAddress:"",
     };
     this.toggleNav = this.toggleNav.bind(this);
   }
 
   toggleNav() {
     this.setState({
-        isNavOpen: !this.state.isNavOpen
+      isNavOpen: !this.state.isNavOpen
     });
   }
 
-  render() {
-      return (
-      <Navbar dark expand="md">
-        <NavbarToggler onClick={this.toggleNav} />
-        <NavbarBrand  href="/">
-          <strong>MetaMob</strong>
-        </NavbarBrand>
-        <Collapse isOpen={this.state.isNavOpen} navbar>
-          <Nav navbar>
-            <NavItem>
-              <NavLink className="nav-link" to="/">
-                Home
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to="/places">
-                Places
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink className="nav-link" to="/campaigns">
-                Campaigns
-              </NavLink>
-            </NavItem>
-          </Nav>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-                <Button outline color="warning">
-                  <span className="fa fa-user"></span> Login
-                </Button>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Navbar>
-    );
+  setWalletAddress=(newWalletAddress)=>{
+    this.setState({
+      walletAddress: newWalletAddress
+    });
   }
+
+  requestAccount = async () => {
+    console.log("Requesting account...");
+  //Check if Meta Mask Extension exists
+    if (window.ethereum) {
+      console.log('detected');
+      try {
+        const accounts = await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+        this.setWalletAddress(accounts[0]);
+        console.log(accounts[0]);
+      } catch(error){
+        console.log("Error connecting...");
+        console.log(error);
+      }
+    } else {
+      alert("Meta Mask not detected");
+    }
+  }
+
+render() {
+  return (
+    <Navbar dark expand="md">
+      <NavbarToggler onClick={this.toggleNav} />
+      <NavbarBrand href="/">
+        <strong>MetaMob</strong>
+      </NavbarBrand>
+      <Collapse isOpen={this.state.isNavOpen} navbar>
+        <Nav navbar>
+          <NavItem>
+            <NavLink className="nav-link" to="/">
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/places">
+              Places
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink className="nav-link" to="/campaigns">
+              Campaigns
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <Button outline color="warning" onClick={this.requestAccount}>
+              <span className="fa fa-user"></span> Login
+            </Button>
+            <h3 style={{color:"white"}}>{this.state.walletAddress}</h3>
+          </NavItem>
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+}
 }
 
 export default Header;
